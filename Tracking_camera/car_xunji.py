916446c79120ed2b0505bef4db49de1191155843
yishujie2 <91115853+yishujie2@users.noreply.github.com>
2024-07-18 00:38:05 +0800

    Update car_xunji.py

--- Tracking_camera/car_xunji.py
+++ Tracking_camera/car_xunji.py
@@ -1,4 +1,5 @@
-THRESHOLD = (18, 61, 14, 89, -2, 63) # Grayscale threshold for dark things...
+Threshold = (18, 61, 14, 89, -2, 63)
+RGB_Threshold = (96, 100, -13, 5, -11, 18)
 
 import sensor, image, time, ustruct, pyb
 from pyb import UART,LED
@@ -8,53 +9,32 @@ LED(2).on()
 LED(3).on()
 
 uart = UART(3,115200)
-uart.init(115200, bits=8, parity=None, stop=1) # init with given parameters
+uart.init(115200, bits=8, parity=None, stop=1)
 
-#识别区域
+# 识别区域
 roi1 =     [(0, 17, 15, 25),        #  左  x y w h
-            (65,17,15,25),# 右
-            (30,0,20,15),#上
-            (0,0,80,60)]#停车
+            (65, 17, 15, 25),# 右
+            (30, 0, 20, 15),#上
+            (0, 0, 80, 60)]#停车
 
+# 数据包
+Run = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x41, 0x26])
+Left = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x42, 0x26])
+Right = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x43, 0x26])
+Stop = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x44, 0x26])
+Crossing = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x45, 0x26]) 
 
-def outuart(x,a,flag):
-    global uart;
-    f_x=0
-    f_a=0
-    if flag==0:
-        if x<0:
-            x=-x
-            f_x=1
-        if a<0:
-            a=-a
-            f_a=1
+N_1 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x31, 0x26])
+N_2 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x32, 0x26])
+N_3 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x33, 0x26])
+N_4 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x34, 0x26])
 
-    if flag==1: #十字
-        x,a,f_x,f_a=(0,0,0,1)
-    if flag==2: #上左
-        x,a,f_x,f_a=(0,0,1,0)
-    if flag==3: #上右
-        x,a,f_x,f_a=(0,0,1,1)
-    if flag==4: #stop
-        x,a,f_x,f_a=(1,1,1,2)
+N_5 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x35, 0x26])
+N_6 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x36, 0x26])
+N_7 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x37, 0x26])
+N_8 = bytearray([0x24, 0x4F, 0x4D, 0x56, 0x38, 0x26])
 
-    data = ustruct.pack("<bbhhhhb",      #格式为俩个字符俩个短整型(2字节)
-                   0x2C,                      #帧头1
-                   0x12,                      #帧头2
-                   int(x), # up sample by 4   #数据1
-                   int(a), # up sample by 4    #数据2
-                   int(f_x), # up sample by 4    #数据1
-                   int(f_a), # up sample by 4    #数据2
-                   0x5B)
-    if flag!=1:
-        uart.write(data);  #必须要传入一个字节数组
-    else:
-
-        for x in range(50):
-            uart.write(data);  #必须要传入一个字节数组
-            time.sleep_ms(1)
-
-
+# 初始化摄像头
 sensor.reset()
 #sensor.set_vflip(True)
 #sensor.set_hmirror(True)
@@ -66,7 +46,7 @@ clock = time.clock()
 
 while(True):
     clock.tick()
-    img = sensor.snapshot().binary([THRESHOLD])
+    img = sensor.snapshot().binary([Threshold])
     line = img.get_regression([(100,100)], robust = True)
 
     left_flag,right_flag,up_flag=(0,0,0)
@@ -88,43 +68,42 @@ while(True):
             print(outdata)
             outuart(rho_err,theta_err,0)
                     
-            if img.find_blobs([(96, 100, -13, 5, -11, 18)],roi=roi1[0]):  #left
+            if img.find_blobs([RGB_Threshold],roi=roi1[0]):  #left
                 #print('left')
                 left_flag=1
-            if img.find_blobs([(96, 100, -13, 5, -11, 18)],roi=roi1[1]):  #right
+            if img.find_blobs([RGB_Threshold],roi=roi1[1]):  #right
                 #print('right')
                 right_flag=1
-            if img.find_blobs([(96, 100, -13, 5, -11, 18)],roi=roi1[2]):  #up
+            if img.find_blobs([RGB_Threshold],roi=roi1[2]):  #up
                 #print('up')
                 up_flag=1
-            #if not img.find_blobs([(96, 100, -13, 5, -11, 18)],roi=roi1[3]):  #stop
+            #if not img.find_blobs([RGB_Threshold],roi=roi1[3]):  #stop
                 ##print('up')
                 #stop_flag=1
             if left_flag==1 and right_flag==1:
-                outuart(0,0,1)
+                uart.write(Crossing)
                 #time.sleep_ms(5)
-                print('shizi')
+                print("Crossing")
                 continue
             if left_flag==1 and up_flag==1:
-                outuart(0,0,2)
-                print('up-left')
+                uart.write(Left)
+                print("Crossing")
                 continue
             if right_flag==1 and up_flag==1:
-                outuart(0,0,3)
-                print('up-right')
+                uart.write(Right)
+                print("Right")
                 continue
             #if stop_flag==1:
-                #outuart(0,0,4)
-                #print('stop')
+                #uart.write(Run)
+                #print("Run")
                 #continue
         else:
             pass
 
     else:
         pass
-
-        outuart(0,0,4)
+                
         #if  not_stop==1:
             #time.sleep_ms(1000)
             #not_stop=0
-        print('stop')
+        #print('stop')
